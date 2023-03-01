1564c2c2ec9a5ee271fd9c8415ff338070b31ed3
Logan Ford <logan@hiyield.co.uk>
2023-03-01 16:18:51 +0000

    reading from table + mysqli install

---
+++ db/main.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3306
+-- Generation Time: Mar 01, 2023 at 03:32 PM
+-- Server version: 8.0.32
+-- PHP Version: 8.1.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_docker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `main`
+--
+
+CREATE TABLE `main` (
+  `name` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `date_created` date NOT NULL,
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `main`
+--
+
+INSERT INTO `main` (`name`, `dob`, `date_created`, `id`) VALUES
+('Logan', '2002-03-22', '2023-03-01', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `main`
+--
+ALTER TABLE `main`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `main`
+--
+ALTER TABLE `main`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- www/index.php
+++ www/index.php
@@ -1,7 +1,22 @@
 <!-- the cdn for the tailwind is in the header - need to install via cli but having issues -->
-<?php include "./src/global/header.php"; ?>
+<?php 
+	include "./src/global/header.php"; 
 
+$connect = mysqli_connect(
+	"db", 
+	"php_docker",
+	"password",
+	"php_docker"
+);
 
-<div class="text-red-500">
-	<?php echo("hello world"); ?>
-</div>
+$table_name = "main";
+
+$query = ("SELECT * FROM $table_name");
+
+$response = mysqli_query($connect, $query);
+
+echo("<strong>$table_name:</strong>");
+while($i = mysqli_fetch_assoc($response)){
+	echo("<p>".$i["name"]."</p>");
+}
+
