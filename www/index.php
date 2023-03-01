ead093113d2c651ef1de0f2bdad79e10c027f758
Logan Ford <logan@hiyield.co.uk>
2023-03-01 17:11:35 +0000

    vue petite with random api pulling name

--- www/index.php
+++ www/index.php
@@ -21,9 +21,40 @@
 	$query = ("SELECT * FROM $table_name");
 	//getting the response
 	$response = mysqli_query($connect, $query);
-	//also ew no thanks 
+	//also ew no thanks no likey 
 	echo("<strong>$table_name:</strong>");
 	while($i = mysqli_fetch_assoc($response)){
 		echo("<p>".$i["name"]."</p>");
 	}
+?>
+
+<div @vue:mounted="mounted()" class="">
+
+</div>
+
+<script type="module">
+	import { createApp } from 'https://unpkg.com/petite-vue?module'
+	createApp({
+		/** data properties */
+		firstName: "",
+
+		async mounted(){
+			const url = "https://random-data-api.com/api/users/random_user?size=1";
+
+			const response = await fetch(url, {
+				method: "GET",
+				cors: "cors",
+				credentials: "same-origin",
+				headers: {
+					"content-type": "application/json"
+				}
+			})
+
+			const person = await response.json();
+			this.firstName = person[0].first_name
+
+			console.log(this.firstName);
+		}	
+	}).mount()
+</script>
 
