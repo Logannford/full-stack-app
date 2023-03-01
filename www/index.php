1a9e0ebc915af04e4455b92f1a877228f7abd17d
Logan Ford <logan@hiyield.co.uk>
2023-03-01 14:50:05 +0000

    trying to install tailwind via cli - installed in cdn for now

---
+++ www/index.php
@@ -0,0 +1,7 @@
+<!-- the cdn for the tailwind is in the header - need to install via cli but having issues -->
+<?php include "./src/global/header.php"; ?>
+
+
+<div class="text-red-500">
+	<?php echo("hello world"); ?>
+</div>
---
+++ www/src/global/header.php
@@ -0,0 +1,5 @@
+<script src="https://cdn.tailwindcss.com"></script> 
+
+<div class="w-full bg-black text-white h-10">
+	header
+</div>
