23d205b7019f401caa44e4842814965d96cc592e
Logan Ford <logan@hiyield.co.uk>
2023-03-02 22:00:32 +0000

    added functions.php

---
+++ www/functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @param string $string
+ * 
+ * Takes in a string, strips it off all malicious tags which could cause 
+ * SQL injection
+ */
+function seo_friendly_url(string $string) {
+	//check if the string contains the special characters
+	if(!preg_match("/[<>\/\'$]/", $string))
+        // If not, capitalize the first letter and return
+        return $string;
+    
+    // Otherwise, remove the special characters and capitalize the first letter
+	$string = strtolower(str_replace(["<", ">", "/", "\'", "$"], '', $string));
+	return ucfirst($string);
+}
+
+//open to the end of time
